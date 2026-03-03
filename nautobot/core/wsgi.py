74fde7c0f772f74e3451db461a6cac2036714250
Jere Julian <github@StageRigger.com>
2026-03-02 21:01:30 -0500

    Ensure a passing test of basic OpenTelemetry traces

--- nautobot/core/wsgi.py
+++ nautobot/core/wsgi.py
@@ -3,7 +3,6 @@ import logging
 from django.core import cache
 from django.core.wsgi import get_wsgi_application
 from django.db import connections
-from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
 import nautobot
 
@@ -35,4 +34,3 @@ except ImportError:
     pass
 
 application = get_wsgi_application()
-application = OpenTelemetryMiddleware(application)
